2d0556cd3ada9f6d923c257887e72cf881108577
Henrique Oliveira <henriqueflashpingo@gmail.com>
2025-12-11 00:17:50 -0300

    feat: create subscription checkout endpoint using stripe for gateway payment

---
+++ src/backend/Schedora.Infrastructure/Services/Payment/StripeSubscriptionPaymentService.cs
@@ -0,0 +1,55 @@
+using Microsoft.Extensions.Logging;
+using Schedora.Domain.Dtos;
+using Schedora.Domain.Enums;
+using Schedora.Domain.Exceptions;
+using Schedora.Domain.Services;
+using Stripe;
+using Stripe.Checkout;
+
+namespace Schedora.Infrastructure.Services.Payment;
+
+public class StripeSubscriptionPaymentService : ISubscriptionPaymentService
+{
+    private readonly ILogger<StripeSubscriptionPaymentService> _logger;
+
+    public StripeSubscriptionPaymentService(ILogger<StripeSubscriptionPaymentService> logger)
+    {
+        _logger = logger;
+    }
+    
+    public async Task<string> CreateCheckoutSession(string customerGatewayId, SubscriptionEnum subscription, string successUrl, string cancelUrl)
+    {
+        var price = StripeSubscriptionPrices.ConvertSubscriptionEnumToPrice(subscription);
+
+        try
+        {
+            var options = new SessionCreateOptions()
+            {
+                Customer = customerGatewayId,
+                Mode = "subscription",
+                PaymentMethodTypes = new List<string>() { "card" },
+                LineItems = new List<SessionLineItemOptions>
+                {
+                    new SessionLineItemOptions()
+                    {
+                        Price = price,
+                        Quantity = 1
+                    }
+                },
+                SuccessUrl = successUrl,
+                CancelUrl = cancelUrl,
+            };
+
+            var service = new SessionService();
+            var session = await service.CreateAsync(options);
+
+            return session.Url;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+            
+            throw;
+        }
+    }
+}
